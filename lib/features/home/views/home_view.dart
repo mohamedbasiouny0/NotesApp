10cc9d31924d37d3280f69f65d881a033198c53b
Mohamed <mido.basiouny123@gmail.com>
2026-02-28 21:25:36 +0200

    Refactor HomeView to use NotesViewBody widget for improved layout structure

--- lib/features/home/views/home_view.dart
+++ lib/features/home/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:test1/features/home/widgets/custom_app_bar.dart';
 import 'package:test1/features/home/widgets/note_item.dart';
+import 'package:test1/features/home/widgets/notes_view_body.dart';
 
 class HomeView extends StatelessWidget {
   HomeView({super.key});
@@ -11,17 +12,7 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            Gap(50),
-            CustomAppBar(onPressed: () {}),
-            Gap(16),
-            NoteItem(),
-          ],
-        ),
-      ),
+      body: NotesViewBody(),
     );
   }
 }
---
+++ lib/features/home/widgets/notes_view_body.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:test1/features/home/widgets/custom_app_bar.dart';
+import 'package:test1/features/home/widgets/note_item.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          Gap(50),
+          CustomAppBar(onPressed: () {}),
+          Gap(16),
+          NoteItem(),
+        ],
+      ),
+    );
+  }
+}
